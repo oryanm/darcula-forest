233cf20e787d4d5816f27b946bafa7abdce6db42
Oryan <oryan.mat@eblock.ca>
2026-05-05 09:01:07 -0400

    Move to subfolders

--- src/main/kotlin/darculaforest/Main.kt
+++ src/main/kotlin/darculaforest/Main.kt
@@ -5,14 +5,19 @@ import java.io.File
 fun main() {
     val outDir = File("darcula")
 
-    File(outDir, "palette.css").writeText(generateCss())
-    File(outDir, "Darcula_Forest.icls").writeText(generateIcls() + "\n")
-    File(outDir, "alacritty.toml").writeText(generateAlacritty() + "\n")
-    File(outDir, "darcula-forest.json").writeText(generateZed() + "\n")
+    val css = File(outDir, "css").apply { mkdirs() }
+    val jetbrains = File(outDir, "jetbrains").apply { mkdirs() }
+    val alacritty = File(outDir, "alacritty").apply { mkdirs() }
+    val zed = File(outDir, "zed").apply { mkdirs() }
+
+    File(css, "palette.css").writeText(generateCss())
+    File(jetbrains, "Darcula_Forest.icls").writeText(generateIcls() + "\n")
+    File(alacritty, "alacritty.toml").writeText(generateAlacritty() + "\n")
+    File(zed, "darcula-forest.json").writeText(generateZed() + "\n")
 
     println("Generated:")
-    println("  darcula/palette.css")
-    println("  darcula/Darcula_Forest.icls")
-    println("  darcula/alacritty.toml")
-    println("  darcula/darcula-forest.json")
+    println("  darcula/css/palette.css")
+    println("  darcula/jetbrains/Darcula_Forest.icls")
+    println("  darcula/alacritty/alacritty.toml")
+    println("  darcula/zed/darcula-forest.json")
 }
